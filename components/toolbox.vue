215906d962ffc1feccd5ebec7359edd25fb2bfda
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-10 02:18:23 +0900

    プロパティや新規要素の追加部分をツールボックスとしてコンポーネントに分離

---
+++ components/toolbox.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="toolbox">
+        <div class="toolbox-switcher">
+            <div>Elements</div>
+            <div>Props</div>
+        </div>
+
+        <div class="toolbox-items">
+            <div class="new-elements" style="display: none">
+                <h2>useful elements</h2>
+                <new-element-list :tag-names="newElementTagNames.useful"></new-element-list>
+                <h2>other elements</h2>
+                <new-element-list :tag-names="newElementTagNames.other"></new-element-list>
+            </div>
+            <element-properties></element-properties>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import newElement from "~/components/newElement.vue";
+    import newElementList from "~/components/newElementList.vue";
+    import elementProperties from "~/components/elementProperties.vue";
+
+
+    export default Vue.extend({
+        name: "toolbox",
+        components: {
+            newElement,
+            newElementList,
+            elementProperties
+        },
+        data() {
+            return {
+                newElementTagNames: {
+                    useful: ["h1", "p", "img", "div", "a", "ul", "li"],
+                    other: ["main", "article", "aside", "video", "canvas", "hoge", "hoge", "hoge", "hoge", "hoge",]
+                }
+            }
+        }
+    });
+</script>
+
+<style scoped lang="scss">
+    .toolbox {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+
+        .toolbox-switcher {
+            height: 48px;
+            display: flex;
+            border-bottom: 2px solid gray;
+
+            & > div {
+                text-align: center;
+                line-height: 48px;
+                flex: 1 1 0;
+
+                &:first-of-type {
+                    border-right: 2px solid gray;
+                }
+            }
+        }
+
+        .toolbox-items {
+            flex: 1 1 0;
+        }
+    }
+</style>
--- pages/editor.vue
+++ pages/editor.vue
@@ -14,23 +14,8 @@
         </div>
 
         <div class="right-sidebar">
-            <div class="editor-toolbox-wrapper">
-                <div class="toolbox-switcher">
-                    <div>Elements</div>
-                    <div>Props</div>
-                </div>
-
-                <div class="toolbox">
-                    <div class="new-elements" style="display: none">
-                        <h2>useful elements</h2>
-                        <new-element-list :tag-names="newElementTagNames.useful"></new-element-list>
-                        <h2>other elements</h2>
-                        <new-element-list :tag-names="newElementTagNames.other"></new-element-list>
-                    </div>
-                    <element-properties></element-properties>
-                </div>
-            </div>
-
+            <!-- TODO: 命名が微妙なので考える -->
+            <toolbox></toolbox>
             <div class="color-palette">
                 <p>color palette</p>
             </div>
@@ -40,25 +25,13 @@
 
 <script lang="ts">
     import Vue from 'vue';
-    import newElement from "~/components/newElement.vue";
-    import newElementList from "~/components/newElementList.vue";
-    import elementProperties from "~/components/elementProperties.vue";
+    import toolbox from "~/components/toolbox.vue";
 
     export default Vue.extend({
         name: "editor",
         components: {
-            newElement,
-            newElementList,
-            elementProperties
+            toolbox
         },
-        data() {
-            return {
-                newElementTagNames: {
-                    useful: ["h1", "p", "img", "div", "a", "ul", "li"],
-                    other: ["main", "article", "aside", "video", "canvas", "hoge", "hoge", "hoge", "hoge", "hoge",]
-                }
-            }
-        }
     });
 </script>
 
@@ -94,32 +67,6 @@
             border-left: 2px solid gray;
             display: flex;
             flex-direction: column;
-
-            .editor-toolbox-wrapper {
-                flex-grow: 1;
-                display: flex;
-                flex-direction: column;
-
-                .toolbox-switcher {
-                    height: 48px;
-                    display: flex;
-                    border-bottom: 2px solid gray;
-
-                    & > div {
-                        text-align: center;
-                        line-height: 48px;
-                        flex: 1 1 0;
-
-                        &:first-of-type {
-                            border-right: 2px solid gray;
-                        }
-                    }
-                }
-
-                .toolbox {
-                    flex: 1 1 0;
-                }
-            }
 
             .color-palette {
                 border-top: 2px solid gray;
