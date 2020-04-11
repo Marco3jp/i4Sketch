7b4710aac15f2a9b00c4b004c61956579ddb95e1
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-12 03:49:45 +0900

    必要最低限のTypesettingElementInterfaceを記述

---
+++ src/interface/TypesettingElement.ts
@@ -0,0 +1,16 @@
+export interface TypesettingElement {
+    // from Node interface
+    readonly childElements: Array<TypesettingElement>
+
+    appendChild(element: TypesettingElement): TypesettingElement
+
+    removeChild(element: TypesettingElement): void
+
+    // from Element interface
+    readonly classList: DOMTokenList
+    id: string
+    readonly tagName: string
+
+    // from HTMLElement interface
+    style: CSSStyleDeclaration
+}
