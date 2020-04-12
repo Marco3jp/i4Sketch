48cf0d3ae092c1334a958c4169acf2b87b5a83f4
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-13 01:03:03 +0900

    imgタグのchildElementに関するTODOを追加

--- src/TImageElement.ts
+++ src/TImageElement.ts
@@ -1,6 +1,7 @@
 import {TElement} from "~/src/TElement";
 import {TypesettingImageElement} from "~/src/interface/TypesettingImageElement";
 
+// TODO: imgタグにはchildElementを持たせられないようにしたほうがいい気がするので考える
 export class TImageElement extends TElement implements TypesettingImageElement {
     alt?: string;
     src: string;
