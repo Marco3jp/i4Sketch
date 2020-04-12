bde8a34fdbcb1ca6d3333f9b78b0d8eb0374873a
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-12 19:39:32 +0900

    TypesettingElementの実装をTElementClassに行った

---
+++ src/TElement.ts
@@ -0,0 +1,31 @@
+import {TypesettingElement} from "~/src/interface/TypesettingElement";
+
+class TElement implements TypesettingElement {
+    readonly childElements: Array<TypesettingElement>;
+    readonly classList: DOMTokenList;
+    id: string;
+    style: CSSStyleDeclaration;
+    readonly tagName: string;
+
+    appendChild(element: TypesettingElement): TypesettingElement {
+        this.childElements.push(element);
+        return element;
+    }
+
+    removeChild(element: TypesettingElement): void {
+        const elementIndex = this.childElements.findIndex(childElement => childElement === element);
+        if (elementIndex === -1) {
+            throw new DOMException("Element was not found", "NotFoundError")
+        } else {
+            this.childElements.splice(elementIndex, 1);
+        }
+    }
+
+    constructor(tagName: string) {
+        this.childElements = [];
+        this.classList = new DOMTokenList();
+        this.id = "";
+        this.style = new CSSStyleDeclaration();
+        this.tagName = tagName;
+    }
+}
