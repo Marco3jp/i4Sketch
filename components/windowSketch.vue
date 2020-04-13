a2f9c07f3bf376927c8e4c999455d163f1fd711f
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-14 02:00:56 +0900

    スケッチブック内でひとつの画面を表すコンポーネントを追加

---
+++ components/windowSketch.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <element-handler v-for="(element, index) in windowData.childElements" :key="index"
+                         :element="element"></element-handler>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue, {PropOptions} from 'vue';
+    import ElementHandler from "~/components/ElementHandler.vue";
+    import {TypesettingWindow} from "~/src/interface/TypesettingWindow";
+
+    export default Vue.extend({
+        name: "windowSketch",
+        components: {ElementHandler},
+        props: {
+            windowData: {
+                type: Object,
+                required: true
+            } as PropOptions<TypesettingWindow>
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
