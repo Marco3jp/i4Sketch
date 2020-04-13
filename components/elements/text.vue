0796d01b1f66803f4ce8b7c48609566fc503fa2c
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-14 01:59:42 +0900

    text要素を追加

---
+++ components/elements/text.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>{{textElementData.value}}</span>
+</template>
+
+<script lang="ts">
+    import Vue, {PropOptions} from 'vue';
+    import {TextElement} from "~/src/interface/TextElement";
+
+    export default Vue.extend({
+        name: "TextElement",
+        props: {
+            textElementData: {
+                type: Object,
+                required: true
+            } as PropOptions<TextElement>
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
