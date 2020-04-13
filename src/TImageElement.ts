6d16091fadcd854ee41e0508f148a91c73aa8ed0
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-14 01:58:48 +0900

    altタグの必須化をclassの方に反映し忘れていたので修正

--- src/TImageElement.ts
+++ src/TImageElement.ts
@@ -3,11 +3,12 @@ import {TypesettingImageElement} from "~/src/interface/TypesettingImageElement";
 
 // TODO: imgタグにはchildElementを持たせられないようにしたほうがいい気がするので考える
 export class TImageElement extends TElement implements TypesettingImageElement {
-    alt?: string;
+    alt: string;
     src: string;
 
-    constructor(src: string) {
+    constructor(src: string, alt: string) {
         super("img");
         this.src = src;
+        this.alt = alt;
     }
 }
