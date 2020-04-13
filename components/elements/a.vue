ec0848c96c0cbfc7357160d35efc61c5e9540af3
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-14 01:57:34 +0900

    TypesettingAnchorElementのaタグを追加

---
+++ components/elements/a.vue
@@ -0,0 +1,26 @@
+<template>
+    <a :href="anchorElementData.href" :id="anchorElementData.id">
+        <element-handler v-for="(element, index) in anchorElementData.childElements" :key="index" :element="element"></element-handler>
+    </a>
+</template>
+
+<script lang="ts">
+    import Vue, {PropOptions} from 'vue';
+    import ElementHandler from "~/components/ElementHandler.vue";
+    import {TypesettingAnchorElement} from "~/src/interface/TypesettingAnchorElement";
+
+    export default Vue.extend({
+        name: "TA",
+        components: {ElementHandler},
+        props: {
+            anchorElementData: {
+                type: Object,
+                required: true
+            } as PropOptions<TypesettingAnchorElement>
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
