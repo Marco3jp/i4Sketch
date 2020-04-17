31be8f3b8528fface13ab47047c5d08dc00214f1
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-17 23:44:19 +0900

    ソースコードの出力時に表示するように要素を追加

--- components/TheSketchbook.vue
+++ components/TheSketchbook.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="sketchbook">
-        <div class="sketchbook-toolbar">
-            <p>edit toolbar</p>
-        </div>
-        <div class="sketchbook-canvas">
+        <the-sketchbook-toolbar></the-sketchbook-toolbar>
+        <div class="sketchbook-canvas" v-if="!isShowCodeView">
             <the-sketchbook-window-item v-for="(window, index) in windowsData" :key="index"
                                         :window-data="window"></the-sketchbook-window-item>
+        </div>
+        <div class="sketchbook-codeview" v-else>
+            <button @click="closeCodeView">close</button>
+            <pre>{{exportedCode}}</pre>
         </div>
     </div>
 </template>
@@ -13,13 +15,25 @@
 <script lang="ts">
     import Vue from 'vue';
     import TheSketchbookWindowItem from "~/components/TheSketchbookWindowItem.vue";
+    import TheSketchbookToolbar from "~/components/TheSketchbookToolbar.vue";
 
     export default Vue.extend({
         name: "TheSketchbook",
-        components: {TheSketchbookWindowItem},
+        components: {TheSketchbookWindowItem, TheSketchbookToolbar},
         computed: {
             windowsData: function () {
                 return this.$store.state.structure.windows;
+            },
+            isShowCodeView: function () {
+                return this.$store.state.view.isShowSourceCode;
+            },
+            exportedCode: function () {
+                return this.$store.state.export.exportedCode;
+            }
+        },
+        methods: {
+            closeCodeView() {
+                this.$store.commit("view/closeExportSourceCodeView");
             }
         }
     });
@@ -31,11 +45,6 @@
         height: 100%;
         display: flex;
         flex-direction: column;
-
-        .sketchbook-toolbar {
-            height: 48px;
-            border-bottom: 2px solid gray;
-        }
 
         .sketchbook-canvas {
             flex-grow: 1;
