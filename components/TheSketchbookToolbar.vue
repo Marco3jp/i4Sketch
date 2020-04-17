17bf24cef51538879c379290f5f9f467f3992159
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-17 23:43:44 +0900

    Sketchbookの上部に配置されるツールバーを追加

---
+++ components/TheSketchbookToolbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sketchbook-toolbar">
+        <source-code-export-button></source-code-export-button>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import SourceCodeExportButton from "~/components/SourceCodeExportButton.vue";
+
+    export default Vue.extend({
+        name: "TheSketchbookToolbar",
+        components: {
+            SourceCodeExportButton
+        }
+    });
+</script>
+
+<style scoped>
+    .sketchbook-toolbar {
+        height: 48px;
+        border-bottom: 2px solid gray;
+    }
+</style>
