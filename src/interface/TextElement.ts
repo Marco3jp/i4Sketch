a3eb0ed2a9198603212fbdea21b78cfa42f3e8fe
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-21 02:21:13 +0900

    textElementのparentElementもreadonlyを削除

--- src/interface/TextElement.ts
+++ src/interface/TextElement.ts
@@ -2,7 +2,7 @@ import {TypesettingElement} from "~/src/interface/TypesettingElement";
 import {TypesettingAnchorElement} from "~/src/interface/TypesettingAnchorElement";
 
 export interface TextElement {
-    readonly parentElement: TypesettingElement | TypesettingAnchorElement;
+    parentElement: TypesettingElement | TypesettingAnchorElement;
     readonly tagName: "#text"
     value: string
 }
