e03033eae4fbbaa2129a33ad8fc412fd25870967
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-20 16:43:32 +0900

    テキスト要素内への挿入を可能にするために要素内を文字ごとに分離

---
+++ components/elements/character.vue
@@ -0,0 +1,21 @@
+<template>
+    <span>{{character}}</span>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: "character",
+        props: {
+            character: String
+        },
+        mounted() {
+            console.log(this.character);
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
--- components/elements/text.vue
+++ components/elements/text.vue
@@ -1,13 +1,18 @@
 <template>
-    <span>{{textElementData.value}}</span>
+    <span>
+        <character v-for="(character, index) in textElementData.value" :character="character"
+                   :key="index"></character>
+    </span>
 </template>
 
 <script lang="ts">
     import Vue, {PropOptions} from 'vue';
     import {TextElement} from "~/src/interface/TextElement";
+    import character from "~/components/elements/character.vue";
 
     export default Vue.extend({
         name: "TextElement",
+        components: {character},
         props: {
             textElementData: {
                 type: Object,
