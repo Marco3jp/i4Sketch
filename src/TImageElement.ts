219385d74064ef7b68dec9109cdf9f74abc08271
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-22 19:06:04 +0900

    TImageElementのsrcとaltをnullを許容するように修正し、categoriesとcontent modelを追加

--- src/TImageElement.ts
+++ src/TImageElement.ts
@@ -1,14 +1,15 @@
 import {TElement} from "~/src/TElement";
 import {TypesettingImageElement} from "~/src/interface/TypesettingImageElement";
+import {CategoriesEnum} from "~/src/enum/categoriesEnum";
 
 // TODO: imgタグにはchildElementを持たせられないようにしたほうがいい気がするので考える
 export class TImageElement extends TElement implements TypesettingImageElement {
-    alt: string;
-    src: string;
+    alt: string | null;
+    src: string | null;
 
-    constructor(src: string, alt: string) {
-        super("img");
-        this.src = src;
-        this.alt = alt;
+    constructor(src?: string, alt?: string) {
+        super("img", [CategoriesEnum.FLOW, CategoriesEnum.PHRASING, CategoriesEnum.EMBEDDED], [CategoriesEnum.NOTHING]);
+        this.src = typeof src !== "undefined" ? src : null;
+        this.alt = typeof alt !== "undefined" ? alt : null;
     }
 }
