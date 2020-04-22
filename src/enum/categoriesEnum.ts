425e052f9418d6e9be01acd601bf0a42ca83ac14
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-22 16:58:09 +0900

    Categoriesにnothingとtransparentを追加し、参考のURLを併記

--- src/enum/categoriesEnum.ts
+++ src/enum/categoriesEnum.ts
@@ -1,9 +1,14 @@
 export enum CategoriesEnum {
+    // https://html.spec.whatwg.org/multipage/dom.html#kinds-of-content
     METADATA,
     FLOW,
     SECTIONING,
     HEADING,
     PHRASING,
     EMBEDDED,
-    INTERACTIVE
+    INTERACTIVE,
+    // https://html.spec.whatwg.org/multipage/dom.html#the-nothing-content-model
+    NOTHING,
+    // https://html.spec.whatwg.org/multipage/dom.html#transparent-content-models
+    TRANSPARENT
 }
