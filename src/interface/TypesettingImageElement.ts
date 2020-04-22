ca11aec22b908032c9b9de1f4dbda142cd1893e8
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-22 19:04:18 +0900

    生成時にsrcとaltを満たすことはできないのでnullableに修正

--- src/interface/TypesettingImageElement.ts
+++ src/interface/TypesettingImageElement.ts
@@ -1,6 +1,6 @@
 import {TypesettingElement} from "~/src/interface/TypesettingElement";
 
 export interface TypesettingImageElement extends TypesettingElement {
-    alt: string
-    src: string
+    alt: string | null
+    src: string | null
 }
