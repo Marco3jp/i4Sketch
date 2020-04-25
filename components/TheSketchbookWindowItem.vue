880f15f7367a9ef378bbf7118c92d1c33f0d416d
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-26 07:37:43 +0900

    element-handlerからelement-creatorに差し替え

--- components/TheSketchbookWindowItem.vue
+++ components/TheSketchbookWindowItem.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
-        <element-handler v-for="(element, index) in windowData.childElements" :key="index"
-                         :element="element"></element-handler>
+        <element-creator v-for="(element, index) in windowData.childElements" :key="index"
+                         :element="element"></element-creator>
     </div>
 </template>
 
 <script lang="ts">
     import Vue, {PropOptions} from 'vue';
-    import ElementHandler from "~/components/ElementHandler.vue";
     import {TypesettingWindow} from "~/src/interface/TypesettingWindow";
+    import ElementCreator from "~/components/ElementCreator";
 
     export default Vue.extend({
         name: "TheSketchbookWindowItem",
-        components: {ElementHandler},
+        components: {
+            ElementCreator,
+        },
         props: {
             windowData: {
                 type: Object,
