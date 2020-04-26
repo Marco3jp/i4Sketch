ab113f331f9b64af18eb3538e1d01fb9ad567e35
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-26 22:28:58 +0900

    imgとaタグにもコンストラクタでオプションを渡すためのインターフェイスを追加

--- src/interface/TypesettingAnchorElement.ts
+++ src/interface/TypesettingAnchorElement.ts
@@ -1,8 +1,13 @@
-import {TypesettingElement} from "~/src/interface/TypesettingElement";
+import {TypesettingElement, TypesettingElementOptions} from "~/src/interface/TypesettingElement";
 
 export interface TypesettingAnchorElement extends TypesettingElement {
     download?: string
+    href?: string
+    target?: "_self" | "_blank" | "_parent" | "_top"
+}
 
+export interface TypesettingAnchorElementOptions extends TypesettingElementOptions {
+    download?: string
     href?: string
     target?: "_self" | "_blank" | "_parent" | "_top"
 }
--- src/interface/TypesettingImageElement.ts
+++ src/interface/TypesettingImageElement.ts
@@ -1,6 +1,11 @@
-import {TypesettingElement} from "~/src/interface/TypesettingElement";
+import {TypesettingElement, TypesettingElementOptions} from "~/src/interface/TypesettingElement";
 
 export interface TypesettingImageElement extends TypesettingElement {
     alt: string | null
     src: string | null
+}
+
+export interface TypesettingImageElementOptions extends TypesettingElementOptions {
+    alt?: string
+    src?: string
 }
