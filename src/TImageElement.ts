d4bad76f96d9d9ca82bf1e47919cdcf7ffd32929
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-26 22:38:34 +0900

    TImageElementのコンストラクタをoptionsに対応

--- src/TImageElement.ts
+++ src/TImageElement.ts
@@ -1,16 +1,21 @@
 import {TElement} from "~/src/TElement";
-import {TypesettingImageElement} from "~/src/interface/TypesettingImageElement";
-import {CategoriesEnum} from "~/src/HTMLSpecReference/enum/categoriesEnum";
+import {TypesettingImageElement, TypesettingImageElementOptions} from "~/src/interface/TypesettingImageElement";
 import {TagNamesEnum} from "~/src/HTMLSpecReference/enum/tagNamesEnum";
+import {getElementCategories} from "~/src/HTMLSpecReference/getElementCategories";
+import {getElementContentModel} from "~/src/HTMLSpecReference/getElementContentModel";
 
 // TODO: imgタグにはchildElementを持たせられないようにしたほうがいい気がするので考える
 export class TImageElement extends TElement implements TypesettingImageElement {
     alt: string | null;
     src: string | null;
 
-    constructor(src?: string, alt?: string) {
-        super(TagNamesEnum.IMG, [CategoriesEnum.FLOW, CategoriesEnum.PHRASING, CategoriesEnum.EMBEDDED], [CategoriesEnum.NOTHING]);
-        this.src = typeof src !== "undefined" ? src : null;
-        this.alt = typeof alt !== "undefined" ? alt : null;
+    constructor(options?: TypesettingImageElementOptions) {
+        super(TagNamesEnum.IMG, {
+            categories: getElementCategories(TagNamesEnum.IMG),
+            contentModel: getElementContentModel(TagNamesEnum.IMG),
+            id: options?.id,
+        });
+        this.src = options?.src ?? null;
+        this.alt = options?.alt ?? null;
     }
 }
