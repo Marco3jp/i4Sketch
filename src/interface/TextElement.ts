f213e4f0e969f95c5d66ea97a447f170d35435cc
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-26 21:49:46 +0900

    テキスト要素にも同様の親要素設定関数を定義

--- src/interface/TextElement.ts
+++ src/interface/TextElement.ts
@@ -4,4 +4,6 @@ export interface TextElement {
     parentElement: NotTextElements | null;
     readonly tagName: "#text"
     value: string
+
+    setParentElement(parentElement: NotTextElements): void
 }
