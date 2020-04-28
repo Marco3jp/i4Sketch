5029bd27cdf3082c1c658f1a1e2628a7d1deea67
marco <28888207+Marco3jp@users.noreply.github.com>
2020-04-29 03:27:10 +0900

    ElementCreatorに、新要素をD&Dしようとした際に許容されていればDrop areaを表示して、Drop時に受け取る実装を追加

--- components/ElementCreator.ts
+++ components/ElementCreator.ts
@@ -1,4 +1,4 @@
-import Vue, {PropOptions, VNode} from 'vue'
+import Vue, {PropOptions, VNode, VNodeData} from 'vue'
 import {TypesettingAnchorElement} from "~/src/interface/TypesettingAnchorElement";
 import {TypesettingImageElement} from "~/src/interface/TypesettingImageElement";
 import {CategoriesEnum} from "~/src/HTMLSpecReference/enum/categoriesEnum";
@@ -6,6 +6,8 @@ import {TagNamesEnum} from "~/src/HTMLSpecReference/enum/tagNamesEnum";
 import {AllElements, NotTextElements} from "~/src/TypeAlias";
 import {TextElement} from "~/src/TextElement";
 import {TElement} from "~/src/TElement";
+import {DisplayOutsideEnum} from "~/src/HTMLSpecReference/enum/displayOutsideEnum";
+import {createElementByTagName} from "~/src/createElementByTagName";
 
 function getAttrs(element: NotTextElements): object {
     switch (element.tagName) {
@@ -37,8 +39,14 @@ export default Vue.component('ElementCreator', {
             required: true
         }
     },
+    data: function () {
+        return {
+            isEnterDrag: false,
+            isDroppable: false,
+        }
+    },
     render: function (createElement): VNode {
-        // テキストノードの振り分け
+        let self = this;
         if (this.element instanceof TextElement) {
             let characterElements: Array<VNode> = [];
             for (let i = 0; i < this.element.value.length; i++) {
@@ -52,24 +60,100 @@ export default Vue.component('ElementCreator', {
                     attrs: getAttrs(this.element)
                 });
             } else {
+                let optionsNewElement: VNodeData = {
+                    on: {
+                        dragover: function () {
+                            self.isEnterDrag = true;
+                            if (typeof self.$store.state.structure.holdingElementCategories !== "undefined") {
+                                self.isDroppable = (self.element as TElement).contentModel.every((category) => {
+                                    return (self.$store.state.structure.holdingElementCategories as Array<CategoriesEnum>).includes(category);
+                                })
+                            }
+                            console.log(self.isEnterDrag, self.isDroppable);
+                            // self.$emit("reverseBubblesMouseEnter");
+                        },
+                        dragleave: function () {
+                            self.isEnterDrag = false;
+                            self.isDroppable = false;
+                            console.log(self.isEnterDrag, self.isDroppable);
+                            // self.$emit("reverseBubblesMouseEnter");
+                        }
+                    },
+                }
+
+                let optionsDropAreaElement: VNodeData = {
+                    style: {
+                        display: self.isEnterDrag && self.isDroppable ? undefined : "none"
+                    },
+                    class: {
+                        "new-element-drop-area-block": self.$store.state.structure.holdingElementDisplayOuteside === DisplayOutsideEnum.BLOCK,
+                        "new-element-drop-area-inline": self.$store.state.structure.holdingElementDisplayOuteside === DisplayOutsideEnum.INLINE,
+                        // "new-element-drop-area-run-in": self.$store.state.structure.holdingElementDisplayOuteside === DisplayOutsideEnum.RUNIN,
+                    }
+                };
+
+                let childElements: Array<VNode> = [];
                 // それ以外の要素を生成して、子がいる場合は更に生成
                 if (this.element.childElements.length !== 0) {
-                    let childElements: Array<VNode> = [];
                     for (let i = 0; i < this.element.childElements.length; i++) {
                         childElements.push(createElement("element-creator", {
                             props: {
-                                element: this.element.childElements[i]
-                            }
+                                element: this.element.childElements[i],
+                                indexOf: i,
+                            },
+                            key: this.element.childElements[i].uuid
                         }));
                     }
-                    return createElement(this.element.tagName, {
-                        attrs: getAttrs(this.element)
-                    }, childElements);
-                } else {
-                    return createElement(this.element.tagName, {
-                        attrs: getAttrs(this.element)
-                    });
                 }
+
+                childElements.unshift(createElement("div", {
+                    ...optionsDropAreaElement,
+                    on: {
+                        drop: function (event: DragEvent) {
+                            event.preventDefault();
+                            console.log("dropped");
+                            self.$store.dispatch("structure/insertAdjustElement", {
+                                position: 'afterbegin',
+                                targetElementUuid: self.element.uuid,
+                                newElement: createElementByTagName(self.$store.state.structure.holdingElementName),
+                                indexOf: self.indexOf
+                            })
+                        },
+                        dragenter: function (event: DragEvent) {
+                            event.preventDefault();
+                        },
+                        dragover: function (event: DragEvent) {
+                            event.preventDefault();
+                        },
+                    }
+                }))
+
+                childElements.push(createElement("div", {
+                    ...optionsDropAreaElement,
+                    on: {
+                        drop: function (event: DragEvent) {
+                            event.preventDefault();
+                            console.log("dropped");
+                            self.$store.dispatch("structure/insertAdjustElement", {
+                                position: 'afterend',
+                                targetElementUuid: self.element.uuid,
+                                newElement: createElementByTagName(self.$store.state.structure.holdingElementName),
+                                indexOf: self.indexOf
+                            })
+                        },
+                        dragenter: function (event: DragEvent) {
+                            event.preventDefault();
+                        },
+                        dragover: function (event: DragEvent) {
+                            event.preventDefault();
+                        },
+                    }
+                }))
+
+                return createElement(this.element.tagName, {
+                    attrs: getAttrs(this.element),
+                    ...optionsNewElement
+                }, childElements);
             }
         } else {
             return createElement('span', "Error Node");
