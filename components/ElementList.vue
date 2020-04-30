b6ef7e716257085d72963060da6bc370d2e34d41
marco <28888207+Marco3jp@users.noreply.github.com>
2020-05-01 03:01:56 +0900

    要素のリストがわかりにくかったので*を(?)に差し替えて、両方のヘッダーにtitleをつけた

--- components/ElementList.vue
+++ components/ElementList.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <h2>sample value</h2>
+        <h2 title="サンプルとなる文章を含んだ状態で追加されるので動きの確認がしやすいです">Sample values (?)</h2>
         <element-list-section :tag-names="elementTagNames.hasSampleValue"></element-list-section>
-        <h2 title="内部的にはちゃんと挿入されるけど表示領域がないのでわかりにくいです">no sample value *</h2>
+        <h2 title="内部的にはちゃんと挿入されるけど表示領域がないのでわかりにくいです">No sample values (?)</h2>
         <element-list-section :tag-names="elementTagNames.noSampleValue"></element-list-section>
     </div>
 </template>
