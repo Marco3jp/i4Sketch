830d6ceb6bde32ce39b8150f9fc8278713fd1507
marco <28888207+Marco3jp@users.noreply.github.com>
2020-05-05 03:05:21 +0900

    サンプルツリーの各要素にidを追記し、imgタグをひとつ追加

--- src/insertSampleTree.ts
+++ src/insertSampleTree.ts
@@ -14,6 +14,7 @@ export function insertSampleTree(store: Store<any>) {
         categories: getElementCategories(TagNamesEnum.DIV),
         childElements: [
             new TElement(TagNamesEnum.H1, {
+                id: "page-header",
                 contentModel: getElementContentModel(TagNamesEnum.H1),
                 categories: getElementCategories(TagNamesEnum.H1),
                 childElements: [
@@ -23,6 +24,7 @@ export function insertSampleTree(store: Store<any>) {
                 ]
             }),
             new TElement(TagNamesEnum.P, {
+                id: "sample-paraphrase",
                 contentModel: getElementContentModel(TagNamesEnum.P),
                 categories: getElementCategories(TagNamesEnum.P),
                 childElements: [
@@ -32,14 +34,17 @@ export function insertSampleTree(store: Store<any>) {
                 ]
             }),
             new TImageElement({
+                id: "sample-image",
                 src: "https://i.imgur.com/s82kM1Y.png",
                 alt: "Arch Linux Logo"
             }),
             new TElement(TagNamesEnum.P, {
+                id: "sanple-link-text",
                 contentModel: getElementContentModel(TagNamesEnum.P),
                 categories: getElementCategories(TagNamesEnum.P),
                 childElements: [
                     new TAnchorElement({
+                        id: "sanple-link",
                         href: "https://example.com",
                         childElements: [
                             new TextElement({
@@ -50,11 +55,12 @@ export function insertSampleTree(store: Store<any>) {
                 ]
             }),
             new TElement(TagNamesEnum.DIV, {
-                id: "",
+                id: "deep-tree-wrapper",
                 contentModel: getElementContentModel(TagNamesEnum.DIV),
                 categories: getElementCategories(TagNamesEnum.DIV),
                 childElements: [
                     new TElement(TagNamesEnum.H2, {
+                        id: "deep-tree-header",
                         contentModel: getElementContentModel(TagNamesEnum.H2),
                         categories: getElementCategories(TagNamesEnum.H2),
                         childElements: [
@@ -64,6 +70,7 @@ export function insertSampleTree(store: Store<any>) {
                         ]
                     }),
                     new TElement(TagNamesEnum.P, {
+                        id: "deep-tree-paraphrase",
                         contentModel: getElementContentModel(TagNamesEnum.P),
                         categories: getElementCategories(TagNamesEnum.P),
                         childElements: [
@@ -76,6 +83,11 @@ export function insertSampleTree(store: Store<any>) {
                                 ]
                             })
                         ]
+                    }),
+                    new TImageElement({
+                        id: "sample-image-second",
+                        src: "https://i.imgur.com/s82kM1Y.png",
+                        alt: "Arch Linux Logo"
                     }),
                 ]
             })
