9a0a7f242824b05d9f359270294513deec412a6d
marco <marco3jp+git@gmail.com>
2020-07-25 02:20:43 +0900

    右サイドバーのコンポーネントが不要になったので削除

--- components/TheRightSidebar.vue
+++ components/TheRightSidebar.vue
@@ -1,22 +1,13 @@
 <template>
     <div class="right-sidebar">
-        <!-- TODO: 命名が微妙なので考える -->
-        <the-right-sidebar-toolbox></the-right-sidebar-toolbox>
-        <div class="color-palette">
-            <p>color palette</p>
-        </div>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
-    import TheRightSidebarToolbox from "~/components/TheRightSidebarToolbox.vue";
 
     export default Vue.extend({
         name: "TheRightSidebar",
-        components: {
-            TheRightSidebarToolbox
-        }
     });
 </script>
 
@@ -27,10 +18,5 @@
         border-left: 2px solid gray;
         display: flex;
         flex-direction: column;
-
-        .color-palette {
-            border-top: 2px solid gray;
-            height: 300px;
-        }
     }
 </style>
