cc43f6d5263c6b2c562508317abf59c450dcbb5d
marco <marco3jp+git@gmail.com>
2020-11-11 00:26:29 +0900

    コメントを追記

--- countStyledElement/index.ts
+++ countStyledElement/index.ts
@@ -2,6 +2,7 @@ const regex = /[#.]/;
 const classIdList: Array<string> = [];
 const styleSheetsErrors: Array<any> = [];
 
+// 全要素数
 let exportMessage = `${document.querySelectorAll("body *:not(script)").length}\t`;
 
 for (let i = 0; i < document.styleSheets.length; i++) {
@@ -16,6 +17,9 @@ for (let i = 0; i < document.styleSheets.length; i++) {
             }
         }
     } catch (e) {
+        // 外部ドメインのCSSを参照できないためここに飛んでくる (クロスオリジンの制約)
+        // その場合はブラウザ側の起動引数とかでセキュリティ機構無効化か、クロスオリジン系の強制有効があれば解決できる
+        // 時代が経つに連れてこのあたり厳しくなっているので、情報を調べる際はいつの情報なのかも気にしてください
         styleSheetsErrors.push(e);
     }
 }
@@ -31,10 +35,14 @@ query += "body *[style]:not(script)"
 
 const elements = document.querySelectorAll(query);
 
+// スタイル設定されていた要素数
 exportMessage += `${elements.length}\t`;
 
+// CSSクエリの数
 exportMessage += `${classIdList.length}`;
 
+// 出力形式は | allElementCount | styledElementCount | cssQueryCount | の順で、
+// エクセルなどにそのまま貼り付け出来るように、タブ文字( \t )で区切っている
 console.log(exportMessage);
 
 console.log(styleSheetsErrors);
